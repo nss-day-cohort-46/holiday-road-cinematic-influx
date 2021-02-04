459057008404f65dca5c907e03c5e5cad24dcf3e
Ccourter11 <courtercalvin@yahoo.com>
2021-02-04 10:37:35 -0600

    began creating data provider for eateries

---
+++ scripts/eateries/Eateries.js
@@ -0,0 +1,3 @@
+export const eateries = (eateriesObj) => {
+  return;
+};
---
+++ scripts/eateries/EateryProvider.js
@@ -0,0 +1,15 @@
+import { defaultKey } from "../Settings.js";
+
+let eateries = [];
+export const useEateries = () => {
+  return eateries.slice();
+};
+
+export const getEateries = () => {
+  return fetch(`http://holidayroad.nss.team/eateries`)
+    .then((response) => response.json())
+    .then((parsedResponse) => {
+      eateries = parsedResponse;
+      console.log(eateries);
+    });
+};
