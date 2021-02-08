1b5bde44122066c83432923ee12ed764ccedb5dd
jashah46 <jatin.shah01@gmail.com>
2021-02-08 14:39:38 -0600

    Added eateries detail

--- scripts/eateries/Eateries.js
+++ scripts/eateries/Eateries.js
@@ -1,5 +1,24 @@
+import "./EateriesDetail.js"
+
 export const Eateries = (eateriesObj) => {
   return `
   <h2 class="eateriesName">Eateries Name: ${eateriesObj.businessName}</h2>
-  `;
-};
+  <button id="eateries--${eateriesObj.id}" class="eateriesButton">Details</button>
+  `
+}
+
+
+const eventHub = document.querySelector(".container")
+
+eventHub.addEventListener("click", event => {
+  if (event.target.id.startsWith("eateries--")) {
+     
+    const [prefix, eateriesId] = event.target.id.split("--")
+    const customEvent = new CustomEvent("EateriesDetailsClicked", {
+      detail: {
+        eateriesId: parseInt(eateriesId)
+      }
+    })
+    eventHub.dispatchEvent(customEvent)
+  }
+})
